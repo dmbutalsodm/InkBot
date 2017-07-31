d8e9a4e261c12ec940475440202bca6dfbcc0774
dmbutalsodm <Kuwar.nagpal@gmail.com>
2017-07-30 20:17:52 -0700

    Changed funfact, now supports addition of additional facts. Fun!

--- commands/fun/funfact.js
+++ commands/fun/funfact.js
@@ -11,6 +11,11 @@ module.exports = class ReplyCommand extends Command {
 	}
 
 	async run(msg) {
-		return msg.say(`Fun fact: Kim Jong-un's name is "Jong-un" \nHis last name is "Kim" \nIf he were in America is would be written as "Jong-un Kim"`); //.say automatically says in the correct channel, the one the command was run from
+		var facts = {
+			0: `Kim Jong-un's name is "Jong-un" \nHis last name is "Kim" \nIf he were in America is would be written as "Jong-un Kim"`,
+			1: `Korean has more vowels than consonants.\n14 consonants, 21 vowels.`
+		}
+		var random = Math.floor(Math.random() * (Object.keys(facts).length));
+		msg.say(`**Fun fact ${random+1} of ${Object.keys(facts).length}**:\n${facts[random]}`);
 	}
 };
