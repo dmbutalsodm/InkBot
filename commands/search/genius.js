881362f0145a1009ce74ed64d43e3317e916bfe7
dmbutalsodm <Kuwar.nagpal@gmail.com>
2017-07-30 22:52:25 -0700

    Removed dead code whoops.

--- commands/search/genius.js
+++ commands/search/genius.js
@@ -20,17 +20,6 @@ module.exports = class ReplyCommand extends Command {
 
 	async run(msg,args) {
         const { query } = args;
-        /*var options = {
-        query: `${query} site:genius.com`,
-        limit: 3
-        };
-        let i = 0
-        scraper.search(options, (err, url) => {
-            if(err) return console.log(err);
-            if(i==0) msg.say(`Here's what I found:\n${url}`);
-            i = 1;
-        });
-        */
         google.resultsPerPage = 1
         google(`${query} site:genius.com`, function (err, res){
             if(err) console.error(err)
