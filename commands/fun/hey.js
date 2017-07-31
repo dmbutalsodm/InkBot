f5fb257b5db05305dcce237fd260ec9a658c8c75
dmbutalsodm <Kuwar.nagpal@gmail.com>
2017-07-30 23:28:33 -0700

    Added more hey for hey.

--- commands/fun/hey.js
+++ commands/fun/hey.js
@@ -11,6 +11,20 @@ module.exports = class ReplyCommand extends Command {
     }
 
 	async run(msg) {
-		return msg.say(`I'm online!`);
+		var greetings = {
+			0: 'Hello!',
+            1: 'Hi!',
+            2: 'Hey there!',
+            3: 'I\'m here!',
+            4: 'Pong!',
+            5: 'Hey!',
+            6: '안녕하세요!',
+            7: 'Yes?',
+            8: 'Ready to work!',
+            9: `hi person hi hi`,
+            10: `Hello jello!`,
+		} 
+        var random = Math.floor(Math.random() * (Object.keys(greetings).length));
+        msg.say(greetings[random]);
 	}
 };
