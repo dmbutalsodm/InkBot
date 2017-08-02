838cf2f93863c048c3f3844429031f07661f0287
dmbutalsodm <Kuwar.nagpal@gmail.com>
2017-08-02 14:57:35 -0700

    Made unsub work.

--- commands/roles/unsub.js
+++ commands/roles/unsub.js
@@ -24,15 +24,13 @@ module.exports = class SayCommand extends Command {
 	async run(msg,args) {
 		const { role } = args;
 		var db = database.get();
-		var roleList = await db.all(`SELECT roleID FROM roles WHERE guildID = '${msg.guild.id}d'`); //gets all the roles that're in the table from this guild as an object
-		roleList = roleList.map((x) => {return x.roleID}); //turns the objects to just an an array with role IDs
+		var roleList = await db.all(`SELECT roleID FROM roles WHERE guildID = '${msg.guild.id}d'`); //Gets all the roles that're in the table from this guild as an object
+		roleList = roleList.map((x) => {return x.roleID}); //Turns the objects to just an an array with role IDs
 		if(!roleList.includes(role.id+'d')) return msg.say("You cannot unsubscribe from this role!"); 
-        if(roleList.includes(role.id+'d')) {
-			msg.member.addRole(role.id).then(() => {
-				return msg.say(`You've unsubscribed from ${role.name}.`);
-			}).catch((x) => {
-				return msg.say('I don\'t have permission for that!');
-			});
-		}
+		msg.member.removeRole(role.id).then(() => { 
+			return msg.say(`You've unsubscribed from **${role.name}**.`); //Sends this regardless.
+		}).catch((x) => {
+			return msg.say('I don\'t have permission for that!'); //If the promise is rejected, since it can only be rejected for permission error, the rejection is handled.
+		});
 	}
 };
